06519d02b9b4ecf2efa0f3bb7ac3cba44975ea14
Tanya <saputo1990@gmail.com>
2020-10-05 00:32:01 +0300

    fixed mobile menu bug for desktop version

--- scripts/script.js
+++ scripts/script.js
@@ -7,19 +7,27 @@ window.onload = function() {
 
   ham.addEventListener("click", toggleHamburger);
   ham.addEventListener("click", toggleNav);
+  window.addEventListener("resize", resetMobileMenu);
+  
+  function resetMobileMenu() {
+    if (document.documentElement.clientWidth >= 1024) {
+      hideNav();
+      ham.classList.remove("is-active")
+    }
+  }
 
   function toggleNav() {
     ham.classList.contains("is-active") ? showNav() : hideNav();
   }
 
-  function showNav () {
+  function showNav() {
     document.getElementById("nav_mobile").style = 'display: flex';
     document.querySelectorAll('.main_wrapper div:not(:first-child)').forEach(el => el.style = 'display: none')
     document.getElementById("footer").style  = 'display: none';
     document.querySelector('header').style = 'display: none';
   }
 
-  function hideNav () {
+  function hideNav() {
     document.getElementById("nav_mobile").style = 'display: none';
     document.querySelectorAll('.main_wrapper div:not(:first-child)').forEach(el => el.style = 'display: block')
     document.getElementById("footer").style  = 'display: block';
