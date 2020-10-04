f69ab5448e31395dc084c41a014ccde4c7d20095
Tanya <saputo1990@gmail.com>
2020-10-04 23:06:56 +0300

    added menu for mobile version

---
+++ scripts/script.js
@@ -0,0 +1,29 @@
+window.onload = function() {
+  let ham = document.querySelector(".hamburger");
+
+  function toggleHamburger() {
+    ham.classList.toggle("is-active")
+  }
+
+  ham.addEventListener("click", toggleHamburger);
+  ham.addEventListener("click", toggleNav);
+
+  function toggleNav() {
+    ham.classList.contains("is-active") ? showNav() : hideNav();
+  }
+
+  function showNav () {
+    document.getElementById("nav_mobile").style = 'display: flex';
+    document.querySelectorAll('.main_wrapper div:not(:first-child)').forEach(el => el.style = 'display: none')
+    document.getElementById("footer").style  = 'display: none';
+    document.querySelector('header').style = 'display: none';
+  }
+
+  function hideNav () {
+    document.getElementById("nav_mobile").style = 'display: none';
+    document.querySelectorAll('.main_wrapper div:not(:first-child)').forEach(el => el.style = 'display: block')
+    document.getElementById("footer").style  = 'display: block';
+    document.querySelector('header').style = 'display: block';
+  }
+}
+
